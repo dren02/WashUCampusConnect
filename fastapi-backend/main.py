f4807cb39da9b75f629f7468bba3deb413bdc664
“Annie <annie.s@wustl.edu>
2024-09-16 18:10:41 -0500

    react to fastapi connection

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from routers.route import router
 
 app = FastAPI()
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # frontend domain
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
-
-
--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/App.css';
 
 const HomePage = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from FastAPI backend
+    fetch('http://127.0.0.1:8000/')
+      .then((response) => response.json())
+      .then((data) => {
+        setEvents(data); // Save the events data to the state
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
+
   return (
     <div className="home-container">
       <h1>Welcome to Campus Website</h1>
       <div className="button-container">
         <Link to="/signup" className="button">Sign Up</Link>
         <Link to="/login" className="button">Log In</Link>
+      </div>
+      
+      <div className="events-container">
+        <h2>Events:</h2>
+        {events.length > 0 ? (
+          <ul>
+            {events.map((event, index) => (
+              <li key={index}>{event.name}</li>
+            ))}
+          </ul>
+        ) : (
+          <p>No events found</p>
+        )}
       </div>
     </div>
   );

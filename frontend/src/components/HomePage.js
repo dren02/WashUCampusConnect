e43976a89c5c9048838fea39f68d66365503a374
“Annie <annie.s@wustl.edu>
2024-09-17 18:36:14 -0500

    signup/login

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import '../styles/App.css';
+import '../styles/HomePage.css';
 
 const HomePage = () => {
   const [events, setEvents] = useState([]);

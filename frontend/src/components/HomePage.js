de32e196e164a50f24d8a3498b24c5a86ac85dd0
dren02 <d.ren@wustl.edu>
2024-11-30 22:01:30 -0600

    Update website title

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -7,7 +7,7 @@ import logo from '../assets/washuLogo.png';
 const HomePage = () => {
   return (
     <div className="home-container">
-      <h1>Welcome to Campus Website</h1>
+      <h1>Welcome to WashU CampusConnect</h1>
       <div className="button-container">
         <Link to="/signup" className="button">Sign Up</Link>
         <Link to="/login" className="button">Log In</Link>
